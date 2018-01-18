765e120dc8b6cffed8daa3d862655efdda58f5c8
Jason Tapia Diaz <jtapiad89@gmail.com>
2018-01-18 13:42:50 -0300

    oso

--- magnoRequisitos/settings.py
+++ magnoRequisitos/settings.py
@@ -78,11 +78,11 @@ WSGI_APPLICATION = 'magnoRequisitos.wsgi.application'
 DATABASES = {
     'default': {
         # 'ENGINE': 'django.db.backends.mysql',
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        # 'NAME': 'requerimientos',
-        # 'USER': 'root',
-        # 'PASSWORD': '081283',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'requerimientos',
+        'USER': 'root',
+        'PASSWORD': '081283',
     }
 }
 
